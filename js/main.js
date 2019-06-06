5d09de0c1534b6c9a2210323ced10180d61ad715
yossaj <yossaj@icloud.com>
2019-06-06 20:14:18 +0100

    added file structure and basic functionality

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+// Fill listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop through empty boxes and add listeners
+
+// Drag Functions
+
+function dragStart() {
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+    this.className = 'fill';
+}
+
+
+
+
+
